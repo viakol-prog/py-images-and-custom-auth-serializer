684e2c4e21f58484b9c9c109bbcc684449bf12de
Viacheslav Kolesnyk <viakol.lokaiv@gmail.com>
2026-06-23 15:08:48 +0300

    images-and-custom-auth-serializer

--- user/views.py
+++ user/views.py
@@ -8,23 +8,22 @@ from user.serializers import CustomAuthTokenSerializer, UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
-    """Створення нового користувача в системі."""
+    """Creating a new user in the system."""
     serializer_class = UserSerializer
 
 
 class CreateTokenView(ObtainAuthToken):
-    """Створення нового токена автентифікації для користувача
-    за допомогою кастомного серіалізатора."""
+    """Creating a new authentication token for a user using a custom serializer."""
     serializer_class = CustomAuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
-    """Управління профілем автентифікованого користувача."""
+    """Authenticated user profile management."""
     serializer_class = UserSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
-        """Повертає об'єкт поточного автентифікованого користувача."""
+        """Returns the current authenticated user object."""
         return self.request.user
